44bd9168b8734b5fffa4bec826da7ad696f16abf
LAZZARONI DANIEL <LAZZARONI.21130>
2026-04-16 09:47:50 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <h1>aggiungi libro</h1>
+        <input type="text" name="titolo" placeholder="titolo" required>
+        <input type="number" min="1900" max="2100" step="1" value="2026" name="anno_pubblicazione" required>
+        <input type="text" name="isbn" placeholder="isbn" required>
+        <select name="autore" required>
+
+        </select>
+    </form>
+</body>
+</html>
